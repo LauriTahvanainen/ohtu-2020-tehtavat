9c5fc1cb0d9e83bf62a12cf1fdce6cdc3cdcd8a8
LauriTahvanainen <48516187+LauriTahvanainen@users.noreply.github.com>
2020-11-09 14:44:17 +0200

    Viikko 3, Tehtävä 1

---
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -0,0 +1,74 @@
+package ohtu;
+
+public class Player {
+
+    private String name;
+    private String team;
+    private String nationality;
+    private String birthdate;
+    private int penalties;
+    private int assists;
+    private int goals;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties = penalties;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    @Override
+    public String toString() {
+        return " | " + name + " | " + team + " | G: " + goals + " | A: " + assists + " | P: " + (assists + goals) + " | PIM: " + penalties + " | ";
+    }
+
+}
