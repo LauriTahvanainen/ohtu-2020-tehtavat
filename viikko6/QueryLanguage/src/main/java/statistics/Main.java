2fa0968e4401a542403054cc44aef7bd7c5089e0
LauriTahvanainen <48516187+LauriTahvanainen@users.noreply.github.com>
2020-12-03 12:07:40 +0200

    Viikko 6 tehtävä 3

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -14,17 +14,26 @@ public class Main {
 //                             new HasAtLeast(5, "assists"),
 //                             new PlaysIn("PHI")
 //        );
-        Matcher m = new And(
-                new Not(new HasAtLeast(1, "goals")),
-                new PlaysIn("NYR")
-        );
-
+//        Matcher m = new And(
+//                new Not(new HasAtLeast(1, "goals")),
+//                new PlaysIn("NYR")
+//        );
 //        Matcher m = new And(
 //                new HasFewerThan(1, "goals"),
 //                new PlaysIn("NYR")
 //        );
-
 //        Matcher m = new And(new All());
+//        Matcher m = new Or(new HasAtLeast(40, "goals"),
+//                new HasAtLeast(60, "assists")
+//        );
+        Matcher m = new And(
+                new HasAtLeast(50, "points"),
+                new Or(
+                        new PlaysIn("NYR"),
+                        new PlaysIn("NYI"),
+                        new PlaysIn("BOS")
+                )
+        );
 
         List<Player> tulos = stats.matches(m);
         System.out.println(tulos.size());
